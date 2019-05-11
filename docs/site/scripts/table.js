41ad1c8d26bb145cb91d7132584b4a414c97a8d7
Tomi <hargitomi97@gmail.com>
2019-05-11 14:39:14 +0200

    many update

--- docs/site/scripts/table.js
+++ docs/site/scripts/table.js
@@ -1,16 +1,19 @@
 $(function () {
   $.ajax({
     type: 'GET',
-    url: 'https://api.myjson.com/bins/16gc1g', // - full
+    //url: 'https://api.myjson.com/bins/m4k2e', // - full
+    url: 'https://api.myjson.com/bins/tgm8m', // test
     //url: 'https://api.myjson.com/bins/1f3pb2', // simplified
+    //url: 'https://raw.githubusercontent.com/hargitomi97/sigstat/master/docs/site/scripts/FullSigStat.json', // from GH
     dataType: 'json',
     success: function (data) {
       var length = Object.keys(data.databases).length;
       var th = "<th class=\"rotate\">";
       var th_end = "</th>";
-      //var rowspan2 = " rowspan=\"2\">";
 
-      $("#asd").append("<th class=\"align-middle\">" + "Verifier's Name" + th_end);
+      $("#asd").append("<th class=\"align-middle\">" + "Verifier's" + "<br>"+ "Name" + th_end);
+      $("#asd").append("<th class=\"align-middle\">" + "Average" + th_end);
+      $("#asd").append("<th class=\"align-middle\">" + "Average" + "<br>" + "Core" + th_end);
       for (var i = 0; i < length; i++) {
         $("#asd").append(th + "<div>" + "<span>" + data.databases[i].Name + "</div>" + "</span>" + th_end);
       }
@@ -21,7 +24,6 @@ $(function () {
       //}
 
       var res_length = Object.keys(data.results).length;
-      console.log(res_length);
 
       var databaseNames = Object.keys(data.results[0]);
       databaseNames.splice(0, 3);
@@ -29,7 +31,9 @@ $(function () {
       var dynamic = "";
 
       for (var i = 0; i < res_length; i++) {
-        dynamic += "<tr>" + "<td>" + data.results[i].verifierName + "</td>"
+        dynamic += "<tr>" + "<td>" + data.results[i].verifierName + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + data.results[i]["Average"].AER + "%"+ "</p>" + "</td>";
+        dynamic += "<td>" + "<p class=\"summary\">" + data.results[i]["Average Core"].AER + "%"+ "</p>" + "</td>";
         for (var j = 0; j < length; j++) {
           dynamic += "<td>" + "<em>" + (data.results[i][databaseNames[j]].AER == null ? " " : (data.results[i][databaseNames[j]].AER).toFixed(2) + "%") + "</em>" + "</td>";
           //"<td>" + (data.results[i][databaseNames[j]].FAR == null ? " " : data.results[i][databaseNames[j]].FAR) + "</td>" +
@@ -50,27 +54,32 @@ $(function () {
 
         var first = data.databases.find(asd => asd.Name === temp);
         //var count = Object.keys(first).length - 1;
-        //console.log(count);
         var test = "";
 
-        for (var i = 1; i <= 6; i++) {
+        for (var i = 1; i <= 5; i++) {
           test += Object.keys(first)[i] + sep + first[Object.keys(first)[i]] + eol
         }
+        test += Object.keys(first)[6] + sep + "<a href=" + first[Object.keys(first)[i]] + " target=\"_blank\">" + first[Object.keys(first)[i]] + "</a>";
+
+        //test = "<a href=http://index.hu target=\"blank\">http://index.hu </a>";
 
         tooltipInstance = $(this).tooltipster({
           contentAsHTML: true,
           content: test,
-          delay: 0,
-          theme: 'tooltipster-light'
-        });
+          delay: 100,
+          theme: 'tooltipster-light',
+          interactive: true
+        })
 
         tooltipInstance.tooltipster('open');
       })
+
       $("table tbody tr td:first-child").on('mouseover', function () {
         var tooltipInstance = null;
         var temp = ($(this).html());
         var sep = ": ";
         var eol = "<br>";
+
 
         var first = data.results.find(asd => asd.verifierName === temp);
         var test = "";
@@ -89,46 +98,69 @@ $(function () {
         tooltipInstance.tooltipster('open');
       })
 
+      $("body").on('mouseover', 'p:not(.tooltipstered)', function () {
+        var rowData = ($(this).parent().parent().find('td:first').text());
+        var currentColumnIndex = $(this).parent().index();
+        console.log(currentColumnIndex);
+        //var db = data.databases[currentColumnIndex];
+        //var final = db[Object.keys(db)[0]];
+        //console.log(rowData);
+        var temp = data.results.find(asd => asd.verifierName === rowData);
+        var almost = "";
+        if(currentColumnIndex === 1){
+          almost = temp[Object.keys(temp)[Object.keys(temp).length-2]];
+        }
+        if(currentColumnIndex === 2){
+          almost = temp[Object.keys(temp)[Object.keys(temp).length-1]];
+        }
+        //console.log(almost);
+
+
+        //var first = data.databases.find(asd => asd.Name === temp);
+        //var count = Object.keys(first).length - 1;
+        var test = "";
+
+        for (var i = 0; i <= 2; i++) {
+          test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
+        }
+        console.log(test);
+
+        //test = "<a href=http://index.hu target=\"blank\">http://index.hu </a>";
+
+        tooltipInstance = $(this).tooltipster({
+          contentAsHTML: true,
+          content: test,
+          delay: 0,
+          theme: 'tooltipster-light',
+        })
+
+        tooltipInstance.tooltipster('open');
+      })
+
       $("body").on('mouseover', 'em:not(.tooltipstered)', function () {
         var rowData = ($(this).parent().parent().find('td:first').text());
-        var currentColumnIndex = $(this).parent().index() - 1;
+        var currentColumnIndex = $(this).parent().index() - 3;
         var db = data.databases[currentColumnIndex];
         var final = db[Object.keys(db)[0]];
-        //console.log(rowData);
-        //console.log(final);
 
-        //var columnData = $(this).parent().parent().parent().parent().parent().parent().index();
-
-        //console.log(Object.keys(data["results"]));
         var temp = data.results.find(asd => asd.verifierName === rowData);
         var almost = temp[Object.keys(temp)[currentColumnIndex + 3]];
-        console.log(Object.keys(almost)[0] + ": " + almost[Object.keys(almost)[0]]);
+        //console.log(Object.keys(almost)[0] + ": " + almost[Object.keys(almost)[0]]);
 
-        /*var dictionary = [];
-        for (var i = 0; i < res_length; i++) {
-          for (var j = 0; j < length; j++) {
-              dictionary.push((data.databases[j].Name + "    " + (data.results[i][databaseNames[j]].AER).toFixed(2) + "%"));
-          }
-        }*/
-        //console.log(currentColumnIndex);
-        //console.log(dictionary);
 
         var tooltipInstance = null;
         var content = null;
 
-        //console.log(columnData);
-
-        // get firstelement of row
 
         var row = data.results.find(asd => asd.verifierName === rowData);
-        //var column = 
         var test = "";
 
         test += Object.keys(row)[0] + ": " + row[Object.keys(row)[0]] + "<br>";
         test += "databaseName: " + final + "<br>";
-        for(var i = 0; i<=2; i++){
+        for (var i = 0; i <= 2; i++) {
           test += Object.keys(almost)[i] + ": " + almost[Object.keys(almost)[i]].toFixed(2) + "%" + "<br>";
         }
+        //console.log(almost[Object.keys(almost)[1]].toFixed(2));
 
 
         tooltipInstance = $(this).tooltipster({
